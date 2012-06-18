fe41ba55f329a517aab9daa4b12a4fccdde1dfb1
Grzegorz Ligas <ligasgr@gmail.com>
2012-06-18 22:21:55 +0200

    Initial Wpf project files created

---
+++ Src/Witchr.Core/Wpf/WitchrApp.xaml.cs
@@ -0,0 +1,16 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Configuration;
+using System.Data;
+using System.Linq;
+using System.Windows;
+
+namespace Witchr.Core.Wpf
+{
+    /// <summary>
+    /// Interaction logic for WitchrApp.xaml
+    /// </summary>
+    public partial class WitchrApp : Application
+    {
+    }
+}
---
+++ Test/Witchr.Core.Tests/Wpf/WitchrAppTest.cs
@@ -0,0 +1,25 @@
+﻿using System;
+using System.Text;
+using System.Collections.Generic;
+using System.Linq;
+using Microsoft.VisualStudio.TestTools.UnitTesting;
+using Witchr.Core.Wpf;
+
+namespace Witchr.Core.Tests.Wpf
+{
+    [TestClass]
+    public class WitchrAppTest
+    {
+
+        [TestMethod]
+        public void ShouldCreateWitchrAppProperly()
+        {
+            //given
+            WitchrApp testee;
+            //when
+            testee = new WitchrApp();
+            //then
+            Assert.IsNotNull(testee);
+        }
+    }
+}
